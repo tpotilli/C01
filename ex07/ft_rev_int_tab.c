372284b68ccfcc1fa2a026ffb2086e60f931819f
Theo Potillion <tpotilli@e3r5p19.clusters.42paris.fr>
2022-09-20 10:52:57 +0200

    42

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,9 +6,11 @@
 /*   By: tpotilli <tpotilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 17:37:41 by tpotilli          #+#    #+#             */
-/*   Updated: 2022/09/20 07:19:43 by tpotilli         ###   ########.fr       */
+/*   Updated: 2022/09/20 10:04:20 by tpotilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	ft_rev_int_tab(int *tab, int size)
 {
@@ -17,13 +19,20 @@ void	ft_rev_int_tab(int *tab, int size)
 
 	i = 0;
 	tmp = 0;
-	size = (-1);
+	size -= 1;
 	while (i <= size)
 	{
 		tmp = tab[i];
-		tab[i] == tab[size];
-		tab[size] == tab[tmp];
+		tab[i] = tab[size];
+		tab[size] = tmp;
 		i++;
 		size--;
 	}
+}
+
+int main()
+{
+    int st[6] = {1,2,3,4,5,6};
+    ft_rev_int_tab(st,6);
+    printf("%d,%d,%d,%d,%d,%d",st[0],st[1],st[2],st[3],st[4],st[5]);
 }
--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,25 +6,42 @@
 /*   By: tpotilli <tpotilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:49:33 by tpotilli          #+#    #+#             */
-/*   Updated: 2022/09/20 09:31:48 by tpotilli         ###   ########.fr       */
+/*   Updated: 2022/09/20 10:48:48 by tpotilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
 	int	a;
+	int	j;
 
 	i = 0;
-	while (i < size)
+	j = 0;
+	while (j < size)
 	{
-		while (tab[i] > tab[i + 1])
+		while (i < size)
 		{
-			a = tab[i];
-			tab[i] = tab[i + 1];
-			tab[i + 1] = a;
-			i--;
+			if(tab[i] >= tab[i + 1])
+			{
+				a = tab[i];
+				tab[i] =tab[i + 1];
+				tab[i + 1] = a;
+				size -= 1;
+			}
+			i++;
 		}
-		i++;
+		j++;
+
+		i = 0;
 	}
+}
+
+int main()
+{
+    int tab[6] = {1,5,-5,0,9,2};
+    ft_sort_int_tab(tab,6);
+    printf("%d,%d,%d,%d,%d,%d",tab[0],tab[1],tab[2],tab[3],tab[4],tab[5]);
 }
