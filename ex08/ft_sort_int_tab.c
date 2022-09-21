adb42d8c56e802f134e57c9d9debbd7862cb6b25
Theo Potillion <tpotilli@e2r7p10.clusters.42paris.fr>
2022-09-21 10:19:46 +0200

    42

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,14 +6,14 @@
 /*   By: tpotilli <tpotilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:49:33 by tpotilli          #+#    #+#             */
-/*   Updated: 2022/09/20 10:58:31 by tpotilli         ###   ########.fr       */
+/*   Updated: 2022/09/20 11:10:05 by tpotilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
-	int	a;
+	int	tmp;
 	int	j;
 
 	i = 0;
@@ -24,9 +24,9 @@ void	ft_sort_int_tab(int *tab, int size)
 		{
 			if (tab[i] >= tab[i + 1])
 			{
-				a = tab[i];
+				tmp = tab[i];
 				tab[i] = tab[i + 1];
-				tab[i + 1] = a;
+				tab[i + 1] = tmp;
 				size -= 1;
 			}
 			i++;
